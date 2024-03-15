71d2f823e6e30ec746a1e380491e956348e65f9f
djordje-jovanovic-dev-telnet <djordje.jovanovic@dev.telnet.rs>
2024-03-15 13:03:30 +0100

    Implement CORS configuration

--- alembic/versions/fecc1063e386_add_content_column_to_posts_table.py
+++ alembic/versions/fecc1063e386_add_content_column_to_posts_table.py
@@ -5,6 +5,7 @@ Revises: 75b7342aad5f
 Create Date: 2024-03-15 08:44:18.782041
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,17 +13,17 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'fecc1063e386'
-down_revision: Union[str, None] = '75b7342aad5f'
+revision: str = "fecc1063e386"
+down_revision: Union[str, None] = "75b7342aad5f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('posts', sa.Column('content', sa.String(), nullable = False))
+    op.add_column("posts", sa.Column("content", sa.String(), nullable=False))
     pass
 
 
 def downgrade() -> None:
-    op.drop_column('posts','content')
+    op.drop_column("posts", "content")
     pass
